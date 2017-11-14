1fe2b4ddfd32a6496635b922e8e1fe7588fdca09
djalal31 <djalal31@hotmail.com>
2017-11-14 10:41:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/home/themedzprofile/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.home.themedzprofile
+
+import android.graphics.BitmapFactory
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // code to make image rounded
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.logo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        // rounded.isCircular = true
+        logo.setImageDrawable(rounded)
+    }
+}
